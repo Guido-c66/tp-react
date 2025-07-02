b8f49fba588bcdb7f421b10994d37808a0020a30
Guido-c66 <gbotta@uade.edu.ar>
2025-07-02 10:20:00 -0300

    Landing.jsx

---
+++ src/componentes/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import "./Landing.css";
+import Hero from "./Hero";
+import Techs from "./Techs";
+
+const Landing = () => (
+  <main className="landing">
+    <Hero />
+    <Techs />
+  </main>
+);
+
+export default Landing;
